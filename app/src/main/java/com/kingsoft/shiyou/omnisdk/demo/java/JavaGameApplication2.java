105c5320e6c504b53f28e86270445bc90fe76bbd
d-txy <tangxiyong1989@gmail.com>
2021-03-03 10:40:37 +0800

    [feature]: init sdk

---
+++ app/src/main/java/com/kingsoft/shiyou/omnisdk/demo/java/JavaGameApplication2.java
@@ -0,0 +1,9 @@
+package com.kingsoft.shiyou.omnisdk.demo.java;
+
+import com.kingsoft.shiyou.omnisdk.project.OmniApplication;
+
+/**
+ * @author tangxiyong
+ */
+public class JavaGameApplication2 extends OmniApplication {
+}
---
+++ app/src/main/java/com/kingsoft/shiyou/omnisdk/demo/kotlin/KotlinGameApplication2.kt
@@ -0,0 +1,10 @@
+package com.kingsoft.shiyou.omnisdk.demo.kotlin
+
+import com.kingsoft.shiyou.omnisdk.project.OmniApplication
+
+/**
+ *
+ * @author tangxiyong
+ */
+class KotlinGameApplication2 : OmniApplication() {
+}
