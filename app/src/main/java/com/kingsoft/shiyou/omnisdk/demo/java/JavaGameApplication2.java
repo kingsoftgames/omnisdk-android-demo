a4efc81bbbb5de0cfd85c942eed55169005516bf
LuXing19870708 <757279327@qq.com>
2021-03-03 18:14:06 +0800

    OMNI API接口示例开发 (#1)
    [feature]: OmniSDK API接口Demo示例开发
    
    OmniSDK API接口账号，支付，社交功能API 接口Demo示例开发

--- app/src/main/java/com/kingsoft/shiyou/omnisdk/demo/java/JavaGameApplication2.java
+++ app/src/main/java/com/kingsoft/shiyou/omnisdk/demo/java/JavaGameApplication2.java
@@ -3,7 +3,8 @@ package com.kingsoft.shiyou.omnisdk.demo.java;
 import com.kingsoft.shiyou.omnisdk.project.OmniApplication;
 
 /**
- * @author tangxiyong
+ * 若游戏应用已经有自身定义的Application并继承自android.app.Application,
+ * 则CP对接方只需将继承类改为 com.kingsoft.shiyou.omnisdk.project.OmniApplication即可
  */
-public class JavaGameApplication2 extends OmniApplication {
-}
+//public class JavaGameApplication2 extends OmniApplication {
+//}
